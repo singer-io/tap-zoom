1a2a5c0012dd656e88c4db2163f4d5e127d42a10
shantanu73 <shantanudhiman25@gmail.com>
2023-03-30 12:55:39 +0000

    Added test all fields

---
+++ tests/test_all_fields.py
@@ -0,0 +1,21 @@
+"""
+Test that with no fields selected for a stream automatic fields are still replicated
+"""
+
+from tap_tester import runner, menagerie, connections
+from tap_tester.base_suite_tests.all_fields_test import AllFieldsTest
+from tap_tester.logger import LOGGER
+from base import ZoomBase
+
+class ZoomAllFieldsTest(AllFieldsTest, ZoomBase):
+    """Test that with no fields selected for a stream automatic fields are still replicated"""
+
+
+    @staticmethod
+    def name():
+        return "tt_zoom_all_fields_test"
+
+    def streams_to_test(self):
+        # streams_to_exclude = {'event_log', 'custom_fields'}
+        return {'users'}
+        # return set(self.expected_metadata().keys())
