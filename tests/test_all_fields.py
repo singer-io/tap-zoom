f44cc2a3720fd1bf095fe6cfff58cdb4227b4aeb
shantanu73 <shantanudhiman25@gmail.com>
2023-04-12 15:09:21 +0000

    Addedd relevant streams and assertions for all fields test

--- tests/test_all_fields.py
+++ tests/test_all_fields.py
@@ -15,7 +15,7 @@ class ZoomAllFieldsTest(AllFieldsTest, ZoomBase):
         return "tt_zoom_all_fields_test"
 
     def streams_to_test(self):
-        return {'webinars', 'webinar_polls'}
+        return {'webinars', 'webinar_polls', 'webinar_registrants', 'users', 'webinar_questions', 'webinar_tracking_sources'}
     
     def test_all_fields_for_streams_are_replicated(self):
         keys_to_remove = set()
@@ -23,7 +23,11 @@ class ZoomAllFieldsTest(AllFieldsTest, ZoomBase):
             with self.subTest(stream=stream):
                 stream_name = self.get_stream_name(stream)
                 if stream == 'webinars':
-                    keys_to_remove = {'occurences', 'tracking_fields', 'recurrence'}
+                    keys_to_remove = {'record_file_id', 'tracking_fields'}
+                elif stream == 'users':
+                    keys_to_remove = {'plan_united_type', 'custom_attributes', 'im_group_ids'}
+                elif stream == 'webinar_tracking_sources':
+                    keys_to_remove = {'registration_count'}
 
                 # gather expectations
                 expected_all_keys = self.selected_fields.get(stream_name, set()) - keys_to_remove
@@ -32,5 +36,7 @@ class ZoomAllFieldsTest(AllFieldsTest, ZoomBase):
                 actual_all_keys_per_record = [set(message['data'].keys()) for message in
                                               self.get_upsert_messages_for_stream(self.synced_records, stream_name)]
 
-                for actual_all_keys in actual_all_keys_per_record:
-                    self.assertSetEqual(expected_all_keys, actual_all_keys)
+                actual_all_keys = set()
+                for record_keys in actual_all_keys_per_record:
+                    actual_all_keys.update(record_keys)
+                self.assertSetEqual(expected_all_keys, actual_all_keys)
