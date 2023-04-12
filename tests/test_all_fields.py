15ac04690e71a2f1f9c9dacb0c741db94d06dbc7
shantanu73 <shantanudhiman25@gmail.com>
2023-04-12 07:01:05 +0000

    Fixed assertions & mmodifications after test runs

--- tests/test_all_fields.py
+++ tests/test_all_fields.py
@@ -15,4 +15,22 @@ class ZoomAllFieldsTest(AllFieldsTest, ZoomBase):
         return "tt_zoom_all_fields_test"
 
     def streams_to_test(self):
-        return set(self.expected_metadata().keys())
+        return {'webinars', 'webinar_polls'}
+    
+    def test_all_fields_for_streams_are_replicated(self):
+        keys_to_remove = set()
+        for stream in self.streams_to_test():
+            with self.subTest(stream=stream):
+                stream_name = self.get_stream_name(stream)
+                if stream == 'webinars':
+                    keys_to_remove = {'occurences', 'tracking_fields', 'recurrence'}
+
+                # gather expectations
+                expected_all_keys = self.selected_fields.get(stream_name, set()) - keys_to_remove
+
+                # gather results
+                actual_all_keys_per_record = [set(message['data'].keys()) for message in
+                                              self.get_upsert_messages_for_stream(self.synced_records, stream_name)]
+
+                for actual_all_keys in actual_all_keys_per_record:
+                    self.assertSetEqual(expected_all_keys, actual_all_keys)
